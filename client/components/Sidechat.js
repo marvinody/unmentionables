9a7aa2c21431f69ff37feb88d4c2534975bdb29c
Manuel Bugallo <marvinody@gmail.com>
2019-06-01 00:13:12 -0400

    some style changes that I will be putting on hold to finish rest of stuff first

--- client/components/Sidechat.js
+++ client/components/Sidechat.js
@@ -1,4 +1,4 @@
-import {Box, Grid, IconButton, Input, Paper} from '@material-ui/core'
+import {Grid, IconButton, Input, Paper} from '@material-ui/core'
 import {makeStyles} from '@material-ui/core/styles'
 import AddIcon from '@material-ui/icons/Add'
 import React, {useState} from 'react'
@@ -8,13 +8,24 @@ const useStyles = makeStyles(theme => ({
   paper: {
     padding: theme.spacing(2),
     margin: 'auto',
+    marginTop: theme.spacing(2),
     maxWidth: 800
   },
   listitem: {
     paddingRight: theme.spacing(2)
   },
-  ul: {
-    listStyle: 'none'
+  outerMessages: {
+    display: 'flex',
+    flexDirection: 'column-reverse',
+    height: 480
+  },
+  messages: {
+    overflow: 'auto',
+    width: '100%',
+    height: 480
+  },
+  span: {
+    margin: 'auto'
   }
 }))
 
@@ -29,30 +40,31 @@ export const Sidechat = props => {
   const [text, setText] = useState('')
   return (
     <Paper className={classes.paper}>
-      <Grid container spacing={2}>
-        <Grid item xs={12} container>
+      <Grid container spacing={1} direction="column">
+        <Grid item xs={12} className={classes.span}>
           <span>Chat</span>
-          <Box>
-            <ul className={classes.ul}>
-              {props.messages.map(msg => {
-                return (
-                  <li key={msg.id}>
-                    {msg.from}: {msg.message}
-                  </li>
-                )
-              })}
-            </ul>
-            <form onSubmit={e => handleSubmit(e, text, setText)}>
-              <Input
-                name="text"
-                value={text}
-                onChange={e => setText(e.target.value)}
-              />
-              <IconButton type="submit" disabled={text.length === 0}>
-                <AddIcon fontSize="small" />
-              </IconButton>
-            </form>
-          </Box>
+        </Grid>
+        <Grid item xs={12} container>
+          <div className={classes.messages}>
+            {props.messages.map(msg => {
+              return (
+                <div key={msg.id}>
+                  {msg.from}: {msg.message}
+                </div>
+              )
+            })}
+          </div>
+          <form onSubmit={e => handleSubmit(e, text, setText)}>
+            <Input
+              name="message"
+              value={text}
+              onChange={e => setText(e.target.value)}
+              autoComplete="new-password"
+            />
+            <IconButton type="submit" disabled={text.length === 0}>
+              <AddIcon fontSize="small" />
+            </IconButton>
+          </form>
         </Grid>
       </Grid>
     </Paper>
