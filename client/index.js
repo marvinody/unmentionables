3d9e87e8e83b9d8b21a06c8aa04eeafd2a1da09c
Manuel Bugallo <marvinody@gmail.com>
2019-06-04 17:23:01 -0400

    global styling changes

--- client/index.js
+++ client/index.js
@@ -2,12 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
 import {Router} from 'react-router-dom'
-import history from './history'
-import store from './store'
 import App from './app'
-
+import './components/stylesheets/body.css'
+import history from './history'
 // establishes socket connection
 import './socket'
+import store from './store'
 
 ReactDOM.render(
   <Provider store={store}>
